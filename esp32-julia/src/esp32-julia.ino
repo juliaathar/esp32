9a7b06841e121d57a152f479e1359efa2907ac59
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 22:23:55 -0300

    new exercises

--- esp32-julia/src/esp32-julia.ino
+++ esp32-julia/src/esp32-julia.ino
@@ -3,6 +3,7 @@
 //exercicio 1 & 2
 char arraydeChar[10]= "char";
 int tamanhoArray = strlen(arraydeChar);
+char texto[11] = "cozinhando";
 
 //exercicio 3
 
@@ -11,10 +12,16 @@ void setup() {
   Serial.begin(9600);
 
    //exercicio 1
-    Serial.print("O tamanho da minha var é: "+ String(tamanhoArray));
+    Serial.printIn("O tamanho da minha var é: "+ String(tamanhoArray));
 
   //exercicio 2
-    Serial.print("Conteúdo da minha var: "+ String(arraydeChar) + "O tamanho da minha var é: "+ String(tamanhoArray));
+    Serial.printIn("Conteúdo da minha var: "+ String(arraydeChar) + "O tamanho da minha var é: "+ String(tamanhoArray));
+
+  //exercicio 3
+    Serial.printIn("Palavras depois:" + String(texto));
+    strcpy(texto, "cozinhei");
+    strcat(texto, "cozinhei");
+    Serial.printIn("Palavras depois:" + String(texto));
 }
  
 void loop(){
