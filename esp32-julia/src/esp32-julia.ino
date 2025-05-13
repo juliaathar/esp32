a35f50f680f53912c10695760ae9bbeacd7e9c8b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-12 21:23:20 -0300

    add: temperature

--- esp32-julia/src/esp32-julia.ino
+++ esp32-julia/src/esp32-julia.ino
@@ -1,29 +1,48 @@
-#include <cstring>
-
-//exercicio 1 & 2
-char arraydeChar[10]= "char";
-int tamanhoArray = strlen(arraydeChar);
-char texto[11] = "cozinhando";
-
-//exercicio 3
-
-
-void setup() {
+#include <DHT.h> // INCLUINDO A BIBLIOTECA DHT NO CÓDIGO
+ 
+#define DHTPIN 19 // DEFININDO O PINO DE ENTRADA DO DHT
+#define DHTMODEL DHT22 // DEFININDO QUAL O MODELO DO DHT QUE VOU UTILIZAR
+ 
+DHT dht (DHTPIN, DHTMODEL); // INCLUINDO TODAS AS VARIAVIES QUE INDIQUEI ANTES NO DHT
+ 
+#define LEDVERMELHO 26
+#define LEDAZUL 27
+ 
+float temp, umid, hic;
+ 
+void setup()
+{
   Serial.begin(9600);
-
-   //exercicio 1
-    Serial.printIn("O tamanho da minha var é: "+ String(tamanhoArray));
-
-  //exercicio 2
-    Serial.printIn("Conteúdo da minha var: "+ String(arraydeChar) + "O tamanho da minha var é: "+ String(tamanhoArray));
-
-  //exercicio 3
-    Serial.printIn("Palavras depois:" + String(texto));
-    strcpy(texto, "cozinhei");
-    strcat(texto, "cozinhei");
-    Serial.printIn("Palavras depois:" + String(texto));
+  dht.begin(); // LIGANDO O DHT
+  pinMode(LEDVERMELHO, OUTPUT);
+  pinMode(LEDAZUL, OUTPUT);
 }
  
-void loop(){
+void loop()
+{
+  temp = dht.readTemperature(); // PEDINDO PARA O DHT REALIZAR A LEITURA DA VARIAVEL DE TEMPERATURA
+  umid = dht.readHumidity(); // PEDINDO PARA O DHT REALIZAR A LEITURA DA VARIAVEL DE UMIDADE
  
+// VERIFICA SE TEVE HOUVE ERRO DE LEITURA NO DHT
+  if (isnan(umid) || isnan(temp)) {
+    Serial.println("\nFalha na leitura!");
+    return;
+  }
+ 
+  Serial.println("--- Temperatura: " + String(temp));
+  Serial.println("--- Umidade: " + String(umid));
+  Serial.println("--- IC: " + String(hic));
+ 
+// FAZENDO O DHT CALCULAR O INDICE DE CALOR DE ACORDO COM AS VARIAVIES DE TEMP E UMID
+  hic = dht.computeHeatIndex(temp, umid, false); // O "FALSE" DIZ PARA O SISTEMA QUE É PARA EXIBIR O RESULTADO EM ºC (CELSIUS)
+ 
+  if (hic > 20) {
+    digitalWrite(LEDVERMELHO, HIGH);
+    Serial.println("\nATENÇÃO: indice de calor alto!");
+  } else {
+    Serial.println("\nATENÇÃO: indice de calor baixo!");
+  }
+ 
+// DELAY MINIMO PARA O DHT É DE 2000 (2seg)
+  delay(2500);
 }
